6c66c699fc5b3ed2cedae390ad2ea51f8734f33b
Zeshan Shahid <z.shahid@hotmail.de>
2023-09-07 13:10:31 +0200

    refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -56,20 +56,6 @@ public class Main {
 
 
 
-        // 1 Recherche ...
-        // 2 Recherche TerminalOperation fehlt
-        // 3 Recherche
-        //Declarative Code: Mit Streams können Sie Ihre Absicht klar und deklarativ ausdrücken, was den Code oft leserlicher und verständlicher macht. Sie beschreiben, was Sie tun möchten (z. B. Filtern, Mapen oder Reduzieren), anstatt wie Sie es tun möchten (z. B. durch das Schreiben von Schleifen).
-        //Kompakt und Ausdrucksstark: Stream-Operationen sind oft kürzer und ausdrucksstärker als gleichwertige Schleifen. Dies kann den Code reduzieren und die Fehleranfälligkeit verringern.
-        //Parallelisierung: Streams bieten integrierte Unterstützung für Parallelverarbeitung. Sie können mit geringem Aufwand parallelisiert werden, um die Leistung auf Mehrkernprozessoren zu verbessern. In Schleifen müssten Sie die Parallelisierung selbst implementieren.
-        //Reusability und Pipelining: Sie können Stream-Operationen wiederverwenden und zu komplexen Pipelines zusammenstellen. Dies erleichtert die Wartung und ermöglicht es, die Operationen klar zu trennen.
-        //Lazy Evaluation: Streams führen Operationen nur aus, wenn tatsächlich ein Ergebnis angefordert wird. Dies ermöglicht die Optimierung und Vermeidung unnötiger Berechnungen.
-        //Funktionale Programmierung: Streams unterstützen einen funktionalen Programmierstil, der die Verarbeitung von Daten in kleinen, unabhängigen Schritten fördert. Dies führt oft zu besser wartbarem und testbarem Code.
-        //Reduzierte Mutabilität: Streams ermutigen dazu, unveränderliche Datenstrukturen zu verwenden und mutierbaren Code zu vermeiden. Dies kann dazu beitragen, Seiteneffekte und unerwünschte Veränderungen des Programmzustands zu reduzieren.
-        //API mit hoher Abstraktion: Die Stream-API bietet viele eingebaute Funktionen, die häufig verwendete Operationen wie Filtern, Sortieren und Reduzieren vereinfachen. Dies erspart Ihnen die Implementierung dieser Operationen von Grund auf.
-        //Bessere Fehlererkennung: In Streams werden Fehler in der Regel während der Pipeline-Ausführung erkannt und behandelt. In Schleifen sind Fehler möglicherweise schwieriger zu finden und zu isolieren.
-        //Dennoch gibt es Situationen, in denen Schleifen immer noch sinnvoll und notwendig sind, insbesondere wenn Sie auf spezifische Indizes oder komplexe Kontrollstrukturen zugreifen müssen. Streams sind ein nützliches Tool in Ihrer Java-Toolbox, aber sie sind nicht immer die beste Wahl für jede Aufgabe. Die Wahl zwischen Schleifen und Streams hängt oft von den Anforderungen und der Lesbarkeit des Codes ab.
-
 
         List<Integer> numbers = Arrays.asList(1, 2, 2, 3, 4, 5, 7, 6, 3, 2, 7, 8, 9);
 
